cf36c3ecbeeb07eb06c1f0932d29482ce38bce81
mikedon <mikedonovan1987@gmail.com>
2014-10-20 23:59:04 -0400

    fixing logout redirect issue.  spring logout now returns 200 rather than 302

--- voodie-server/src/main/java/com/voodie/web/SimpleCORSFilter.java
+++ voodie-server/src/main/java/com/voodie/web/SimpleCORSFilter.java
@@ -12,7 +12,6 @@ public class SimpleCORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        System.out.println("in SimpleCorsFilter filter");
         HttpServletResponse response = (HttpServletResponse) resp;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
